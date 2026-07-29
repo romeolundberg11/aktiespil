f3b01d00aa1f6f1f6a5a3a710bb649118b359bb6
romeolundberg11 <romeolundberg11@gmail.com>
2026-07-29 13:10:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "boersspil-cache-v1";
+const CACHE_NAME = "boersspil-cache-v2";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
